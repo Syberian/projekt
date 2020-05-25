5daef69a547a50b2e7d549227f851af475a19e20
Syberian <mixux98@gmail.com>
2020-05-25 20:38:07 +0200

    init

---
+++ app/src/main/java/com/example/ksiazka/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ksiazka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void recipesClicked(View view) {
+    }
+
+    public void partsClicked(View view) {
+    }
+
+    public void shoppingClicked(View view) {
+    }
+
+    public void quitClicked(View view) {
+    }
+}
